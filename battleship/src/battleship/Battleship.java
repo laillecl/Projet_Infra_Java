abd5128dc3948482c3359d5d5cb12b8ecb37046e
Painochoc <clement.laille@outlook.fr>
2019-12-20 10:13:20 +0100

    battleship boat + gamemanager

--- battleship/src/battleship/Battleship.java
+++ battleship/src/battleship/Battleship.java
@@ -4,7 +4,7 @@ public class Battleship {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		GameManager gm = new GameManager();
 	}
 
 }
---
+++ battleship/src/battleship/GameManager.java
@@ -0,0 +1,36 @@
+package battleship;
+
+import battleship.components.Boat;
+import battleship.ihm.Grid;
+
+public class GameManager {
+	
+	// Constants
+	public static  int gridSize = 10;
+	
+	public static int carrierAmount = 1;
+	public static int battleshipAmount = 1;
+	public static int cruiserAmount = 1;
+	public static int submarineAmount = 1;
+	public static int destroyerAmount = 1;
+	
+	// Keep track of boats
+	private Boat[] Player1Boats;
+	private Boat[] Player2Boats;
+	
+	// Grid instance
+	private Grid grid;
+	
+	// TODO Players
+	
+	GameManager()
+	{
+		// Instanciate grid
+		// Instanciate p1, p2 boats
+		
+	}
+	
+	
+	
+	
+}
