8c50751483cd20368023ce792fc2460700a7f72f
SachaBrd <sacha.bourdeau@reseau.eseo.fr>
2019-12-23 21:54:05 +0100

    NumerosBombes

--- demineur/src/components/Grille.java
+++ demineur/src/components/Grille.java
@@ -5,7 +5,12 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Grille extends JPanel implements MouseListener{
@@ -18,24 +23,37 @@ public class Grille extends JPanel implements MouseListener{
 	public int size;
 	public int mouseX;
 	public int mouseY;
-	int xMat;
-	int yMat;
+	public int xMat;
+	public int yMat;
+	private BufferedImage image;
 	
-	public static final int GRID_SIZE = 10;
-	public static final int X_ORIGIN = 40; // X coordinate of the top left
-	public static final int Y_ORIGIN = 40; // Y coordinate of the top left
-	public static final int TILE_SIZE = 39; // Size of the tile spaces
+	public static final int GRID_SIZE = 15;
+	public static final int X_ORIGIN = 20; // X coordinate of the top left
+	public static final int Y_ORIGIN = 20; // Y coordinate of the top left
+	public static final int TILE_SIZE = 29; // Size of the tile spaces
 	public static final int BORDER_SIZE = 1; // size of the border between spaces
 	
 	public Grille(int size) {
 		this.matrice = new Case[size][size];
 		construireMatrice();
+
+//		try {
+//			image = ImageIO.read(new File("flagIcon.png"));
+//		} catch (IOException e) {
+//			System.out.println("Failed to load image");
+//		}
 	}
 	
 	public Grille() {
 		this.size = GRID_SIZE;
 		this.matrice = new Case[size][size];
 		construireMatrice();
+		
+//		try {
+//			image = ImageIO.read(new File("flagIcon.png"));
+//		} catch (IOException e) {
+//			System.out.println("Failed to load image");
+//		}
 	}
 	
 	public Case[][] getMatrice() {
@@ -47,26 +65,54 @@ public class Grille extends JPanel implements MouseListener{
 	}
 	
 	public void construireMatrice(){
+		// placement aleatoire des bombes
+		Random rand = new Random();
 		for (int i = 0; i < this.size; i++) {
 			for (int j = 0; j < this.size; j++) {
 				if(i == 0 || j == 0) {
-					this.matrice[i][j] = new Case(X_ORIGIN + i*TILE_SIZE, Y_ORIGIN + j*TILE_SIZE,1);
+					this.matrice[i][j] = new Case(X_ORIGIN + i*(TILE_SIZE+1), Y_ORIGIN + j*(TILE_SIZE+1),1);
 				}
 				else if(i == this.size -1 || j == this.size -1) {
-					this.matrice[i][j] = new Case(X_ORIGIN + i*TILE_SIZE, Y_ORIGIN + j*TILE_SIZE,1);
+					this.matrice[i][j] = new Case(X_ORIGIN + i*(TILE_SIZE+1), Y_ORIGIN + j*(TILE_SIZE+1),1);
 				}
 				else {
-					this.matrice[i][j] = new Case(X_ORIGIN + i*TILE_SIZE, Y_ORIGIN + j*TILE_SIZE);
+					this.matrice[i][j] = new Case(X_ORIGIN + i*(TILE_SIZE+1), Y_ORIGIN + j*(TILE_SIZE+1));
+					if (rand.nextInt(100) <20) {
+						this.matrice[i][j].setTileType(2);
+					}
 				}
 			}
 		}
 	}
 	
-//	public int[] convertirCoordonneesMatrice() {
-//		int xMat = mouseX - X_ORIGIN / TILE_SIZE;
-//		int yMat = mouseY - Y_ORIGIN / TILE_SIZE;
-//		return [xMat,YMat];
-//	}
+	public int caseAdjacenteBombe(int i,int j) {
+		int compteurBombes = 0;
+		if (this.matrice[i-1][j-1].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i-1][j].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i-1][j+1].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i][j+1].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i][j-1].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i+1][j-1].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i+1][j].getTileType() == 2){
+			compteurBombes++;
+		}
+		if (this.matrice[i+1][j+1].getTileType() == 2){
+			compteurBombes++;
+		}
+		return compteurBombes;		
+	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -84,18 +130,46 @@ public class Grille extends JPanel implements MouseListener{
 				}
 				else {
 					g2.setColor(Color.DARK_GRAY);
+					
+					// Est-ce que la case est cliquée ?
 					if (i == xMat && j == yMat) {
 						this.matrice[i][j].setIsClicked(true);
 					}
+					
+					// Si oui on change sa couleur
 					if(this.getCase(i, j).isClicked) {
 						g2.setColor(Color.LIGHT_GRAY);
+						
+						// Si c'est un bombe, icone bombe
+						if(this.getCase(i, j).getTileType() == 2) {
+							g2.setColor(Color.red);
+							// g2.drawImage(image, X_ORIGIN + i*(TILE_SIZE+1), Y_ORIGIN + j*(TILE_SIZE+1), this);
+						}
 					}
+					
+					// Sinon on laisse un rectangle gris
 					g2.fillRect(X_ORIGIN + i*(TILE_SIZE+1), Y_ORIGIN + j*(TILE_SIZE+1), TILE_SIZE, TILE_SIZE);
 					
+					int cpt = caseAdjacenteBombe(i,j);
+					String str = String.valueOf(cpt);
+					
+					if(cpt != 0) {
+						if(this.getCase(i, j).isClicked) {
+							if(this.getCase(i,j).getTileType() != 2) {
+								g2.setColor(Color.black);
+								g2.drawString(str, X_ORIGIN + i*(TILE_SIZE+1) + TILE_SIZE/2, Y_ORIGIN + j*(TILE_SIZE+1) + TILE_SIZE/2);
+							}
+						}
+					}
 				}
 			}
 		}
 		
+	}
+
+	private char[] toString(int cpt) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -104,8 +178,8 @@ public class Grille extends JPanel implements MouseListener{
 		mouseY = e.getY();
 		xMat = (mouseX - X_ORIGIN)/(TILE_SIZE + BORDER_SIZE);
 		yMat = (mouseY - Y_ORIGIN)/(TILE_SIZE + BORDER_SIZE) -1;
-		System.out.println("Un clic en " + mouseX + " et " + mouseY + "Les coordonnees sont " + xMat + " et " + yMat);
-		System.out.println("");
+		System.out.println("Un clic en " + mouseX + " et " + mouseY + " Les coordonnees sont " + xMat + " et " + yMat);
+		System.out.println("Les bombes adjactentes sont : " + caseAdjacenteBombe(xMat,yMat));
 	}
 
 	@Override
